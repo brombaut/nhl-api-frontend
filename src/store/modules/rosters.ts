7a9aacf44cef1a75efa36596832fa72439224b6e
Ben <ben.rombaut@eigen.io>
2020-04-04 20:01:46 -0300

    Load rosters in when a team becomes selected

---
+++ src/store/modules/rosters.ts
@@ -0,0 +1,51 @@
+import {
+  VuexModule,
+  Module,
+  Mutation,
+  Action,
+  getModule
+} from "vuex-module-decorators";
+import store from "@/store";
+import { Roster } from "@/types/store-types/roster";
+import { teamsApi } from "@/services/nhl-api-adapter/teams-api";
+import { TeamsModule } from "./teams";
+
+export interface RostersState {
+  rosters: Array<Roster>;
+}
+
+@Module({ dynamic: true, store, name: "rosters" })
+class Rosters extends VuexModule implements RostersState {
+  private _rosters: Array<Roster> = [];
+
+  public get rosters(): Array<Roster> {
+    return this._rosters;
+  }
+
+  public get teamRoster() {
+    return (teamId: number) =>
+      this._rosters.find((roster: Roster) => roster.teamId === teamId)
+        ?.rosterRelations || null;
+  }
+
+  public get selectedTeamRoster() {
+    return (
+      this._rosters.find(
+        (roster: Roster) => roster.teamId === TeamsModule.selectedTeamId
+      )?.rosterRelations || null
+    );
+  }
+
+  @Action
+  public async loadTeamRoster(teamId: number): Promise<void> {
+    const roster: Roster = await teamsApi.getTeamRoster(teamId);
+    this.addRoster(roster);
+  }
+
+  @Mutation
+  private addRoster(roster: Roster) {
+    this._rosters.push(roster);
+  }
+}
+
+export const RostersModule = getModule(Rosters);
