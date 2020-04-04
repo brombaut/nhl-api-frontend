08ad4d6124a90e4c0929f552ac00b31044dbb432
Ben <ben.rombaut@eigen.io>
2020-04-04 20:22:18 -0300

    Players module. Loading in a roster relation will trigger the associated player record to be loaded in

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,6 +4,7 @@ import { TeamsState } from "./modules/teams";
 import { DivisionsState } from "./modules/divisions";
 import { ConferencesState } from "./modules/conferences";
 import { RostersState } from "./modules/rosters";
+import { PlayersState } from "./modules/players";
 
 Vue.use(Vuex);
 
@@ -12,6 +13,7 @@ export interface RootState {
   divisions: DivisionsState;
   conferences: ConferencesState;
   rosters: RostersState;
+  players: PlayersState;
 }
 
 export default new Vuex.Store<RootState>({});
