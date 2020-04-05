0624cc0fce09ec86e196baf99ca7eadc1c7703c6
Ben <ben.rombaut@eigen.io>
2020-04-05 11:15:32 -0300

    Navbar seems to be working

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <TeamSelector />
     <router-view />
   </div>
 </template>
@@ -15,8 +12,13 @@ import { TeamsModule } from "./store/modules/teams";
 import { DivisionsModule } from "./store/modules/divisions";
 import { ConferencesModule } from "./store/modules/conferences";
 import { StandingsModule } from "./store/modules/standings";
+import TeamSelector from "@/components/TeamSelector.vue";
 
-@Component
+@Component({
+  components: {
+    TeamSelector
+  }
+})
 export default class App extends Vue {
   public loadInitialData(): void {
     ConferencesModule.loadConferences();
@@ -25,31 +27,39 @@ export default class App extends Vue {
     StandingsModule.loadStandingsTypes();
   }
 
-  // mounted() {
-  //   this.loadInitialData();
-  // }
+  mounted() {
+    this.loadInitialData();
+  }
 }
 </script>
 
 <style lang="scss">
+html,
+body {
+  margin: 0;
+  height: 100%;
+  width: 100%;
+  overflow-x: hidden;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
+  // color: #2c3e50;
+  background-color: #1c2532;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  // position: relative;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
 </style>
