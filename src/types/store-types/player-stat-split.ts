264c8c78ca4255991a0c94d55a4bf5908e648b84
Ben <ben.rombaut@eigen.io>
2020-04-25 19:46:34 -0300

    Adding more player details

--- src/types/store-types/player-stat-split.ts
+++ src/types/store-types/player-stat-split.ts
@@ -1,7 +1,8 @@
 import { NhlApiPlayerStatSplit } from "../nhl-api-types/nhl-api-player-stat-split";
+import { MyMap } from "../data-types/map";
 
 export class PlayerStatSplit {
-  stat: object;
+  stat: MyMap<string | number>;
   season?: string;
 
   constructor(nhlApiPlayerStatSplit: NhlApiPlayerStatSplit) {
